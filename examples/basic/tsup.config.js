d41c43d6e41995c973c06c0f20c7b3ebd0260743
neverUsedGithub <hulyevagyok420@gmail.com>
2023-03-22 11:07:55 +0100

    removed examples

--- examples/basic/tsup.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup'
-
-export default defineConfig({
-  entry: [ "main.jsx" ],
-  esbuildOptions(options, context) {
-    options.jsxFactory = "Instant.createElement";
-    options.jsxFragment = "Instant.Fragment";
-  }
-})
